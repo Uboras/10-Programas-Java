1be996b86c63ed34fb8f63853232b9837fcb50d4
Uboras <129528183+Uboras@users.noreply.github.com>
2023-12-14 02:25:14 -0300

    first Program

---
+++ Program1/src/program1/Program1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package program1;
+
+/**
+ *
+ * @author Hernan Manera
+ Vamos a escribir "¡Hola Mundo!" en consola..
+ * Let's write "Hello World!" on console...
+ */
+           
+ public class Program1{
+     
+     
+ public static void main(String args[]){
+ 
+     //metodo del sistema para mostrar en consola una variable o texto. 
+     //System method to display a variable or text in the console.
+     System.out.println("Hola Mundo");
+
+ }
+}
+   //gracias por mirar, sigmos adelante
+//thanks for watching, let's move on
+    
+
